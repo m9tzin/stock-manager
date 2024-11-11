136eb0b6f9f3ba9f6daa34046b46e058cc6618a5
m9tzin <matheusmrno@gmail.com>
2024-11-11 08:05:17 -0300

    first step

---
+++ gerenciador.c
@@ -0,0 +1,29 @@
+/* Gerenciador de Estoque
+ *
+ * Estoque.c
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Produto{
+	int ID;
+};c
+
+typedef struct Produto Produto;
+
+struct Lista{
+	struct Produto *primeiro;
+	struct Produto *ultimo;
+};
+
+typedef struct Lista Lista;
+
+
+int main(void){
+	return 0;
+}
+
+
